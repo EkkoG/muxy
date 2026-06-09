df2331e0ff977adb6efa3b6f30b0f50d212a1e42
Ekko <beijiu572@gmail.com>
2026-06-09 18:11:34 +0800

    Fix native SSH I/O lifecycle and host key matching

--- Muxy/Views/Terminal/GhosttyTerminalNSView.swift
+++ Muxy/Views/Terminal/GhosttyTerminalNSView.swift
@@ -235,6 +235,8 @@ final class GhosttyTerminalNSView: NSView {
                             guard let self, !self.processExitHandled else { return }
                             self.processExitHandled = true
                             self.onProcessExit?()
+                        },
+                        onFinished: { _ in
                         }
                     )
                 )
